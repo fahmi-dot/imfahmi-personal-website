9998f45894236d28a44f191f0bd36de8f0a21d0d
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-21 13:03:28 +0700

    feat: add component files

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navbar'
+export { default as Hero } from './Hero'
+export { default as About } from './About'
+export { default as Skills } from './Skills'
+export { default as Portfolios } from './Portfolios'
+export { default as Footer } from './Footer'
+export { default as MenuButton } from './MenuButton'
