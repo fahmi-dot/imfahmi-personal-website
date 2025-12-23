d3bab4058ac45a4ce1626270e6ee8d6081e70eee
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-23 13:32:31 +0700

    style: update Home UI styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,19 +12,19 @@ const Home = () => {
         <main className='w-5/6'>
           <div className='container'>
             <div className='container-box'>
-              <section id='hero' className='pb-5' data-aos='fade-up'>
+              <section id='hero' className='border-none mt-0 pt-0 pb-5' data-aos='fade-up'>
                 <Hero />
               </section>
-              <section id='about' className='my-5 py-5' data-aos='fade-up'>
+              <section id='about' data-aos='fade-up'>
                 <About />
               </section>
-              <section id='skills' className='my-5 py-5' data-aos='fade-up'>
+              <section id='skills' data-aos='fade-up'>
                 <Skills />
               </section>
-              <section id='portfolios' className='my-5 py-5' data-aos='fade-up'>
+              <section id='portfolios' data-aos='fade-up'>
                 <Portfolios />
               </section>
-              <section id='footer' className='pt-5' data-aos='fade-up'>
+              <section id='footer' className='my-0 pt-5 pb-0' data-aos='fade-up'>
                 <Footer />
               </section>
             </div>
