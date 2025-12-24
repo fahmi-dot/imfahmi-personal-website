e249870466b5540c0cd2d28a98144b75cca7a230
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-24 20:19:04 +0700

    style: add responsive UI in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,28 +6,26 @@ const Home = () => {
     <div>
       <MenuButton />
       <div className='flex justify-end'>
-        <header className='w-1/6'>
+        <header>
           <Navbar />
         </header>
-        <main className='w-5/6'>
-          <div className='container'>
-            <div className='container-box'>
-              <section id='hero' className='border-none mt-0 pt-0 pb-5' data-aos='fade-up'>
-                <Hero />
-              </section>
-              <section id='about' data-aos='fade-up'>
-                <About />
-              </section>
-              <section id='skills' data-aos='fade-up'>
-                <Skills />
-              </section>
-              <section id='portfolios' data-aos='fade-up'>
-                <Portfolios />
-              </section>
-              <section id='footer' className='my-0 pt-5 pb-0' data-aos='fade-up'>
-                <Footer />
-              </section>
-            </div>
+        <main className='w-full lg:w-5/6'>
+          <div className='container-box'>
+            <section id='hero' className='border-none mt-0 pt-0 pb-5' data-aos='fade-up'>
+              <Hero />
+            </section>
+            <section id='about' data-aos='fade-up'>
+              <About />
+            </section>
+            <section id='skills' data-aos='fade-up'>
+              <Skills />
+            </section>
+            <section id='portfolios' data-aos='fade-up'>
+              <Portfolios />
+            </section>
+            <section id='footer' className='my-0 pt-5 pb-0' data-aos='fade-up'>
+              <Footer />
+            </section>
           </div>
         </main>
       </div>
